05a01f6149bda1d4572c8e789aa6041443935ba1
DavidJuarez21 <152940893+DavidJuarez21@users.noreply.github.com>
2023-12-04 21:19:30 -0700

    Add files via upload

---
+++ scritps.js
@@ -0,0 +1,108 @@
+let mostrador = document.getElementById("mostrador");
+let seleccion = document.getElementById("seleccion");
+let imgSeleccionada = document.getElementById("img");
+let modeloSeleccionado = document.getElementById("modelo");
+let descripSeleccionada = document.getElementById("descripcion");
+let precioSeleccionado = document.getElementById("precio");
+
+function cargar(item){
+    quitarBordes();
+    mostrador.style.width = "60%";
+    seleccion.style.width = "40%";
+    seleccion.style.opacity = "1";
+    item.style.border = "2px solid red";
+
+    imgSeleccionada.src = item.getElementsByTagName("img")[0].src;
+
+    modeloSeleccionado.innerHTML =  item.getElementsByTagName("p")[0].innerHTML;
+
+    descripSeleccionada.innerHTML = "Descripción del modelo ";
+
+    precioSeleccionado.innerHTML =  item.getElementsByTagName("span")[0].innerHTML;
+
+
+}
+function cerrar(){
+    mostrador.style.width = "100%";
+    seleccion.style.width = "0%";
+    seleccion.style.opacity = "0";
+    quitarBordes();
+}
+function quitarBordes(){
+    var items = document.getElementsByClassName("item");
+    for(i=0;i <items.length; i++){
+        items[i].style.border = "none";
+    }
+}
+
+
+
+(function(){
+    const listElements = document.querySelectorAll('.menu__item--show');
+    const list = document.querySelector('.menu__links');
+    const menu = document.querySelector('.menu__hamburguer');
+
+    const addClick = ()=>{
+        listElements.forEach(element =>{
+            element.addEventListener('click', ()=>{
+
+                
+                let subMenu = element.children[1];
+                let height = 0;
+                element.classList.toggle('menu__item--active');
+
+
+                if(subMenu.clientHeight === 0){
+                    height = subMenu.scrollHeight;
+                }
+
+                subMenu.style.height = `${height}px`;
+
+            });
+        });
+    }
+
+    const deleteStyleHeight = ()=>{
+        listElements.forEach(element=>{
+
+            if(element.children[1].getAttribute('style')){
+                element.children[1].removeAttribute('style');
+                element.classList.remove('menu__item--active');
+            }
+
+        });
+    }
+
+
+    window.addEventListener('resize', ()=>{
+        if(window.innerWidth > 800){
+            deleteStyleHeight();
+            if(list.classList.contains('menu__links--show'))
+                list.classList.remove('menu__links--show');
+
+        }else{
+            addClick();
+        }
+    });
+
+    if(window.innerWidth <= 800){
+        addClick();
+    }
+
+    menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show'));
+
+
+
+})();
+
+
+// Código JavaScript para gestionar la visibilidad de los submenús
+document.addEventListener('DOMContentLoaded', function () {
+    var menuItems = document.querySelectorAll('.menu > li');
+
+    menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener('click', function () {
+            this.querySelector('.submenu').classList.toggle('show');
+        });
+    });
+});
